007044e9309fca587500dfa1964f62a4c11872fc
Aditya Aryan <adityaaryan77@gmail.com>
2023-04-25 05:05:09 +0530

    Add success and failure page

--- app.js
+++ app.js
@@ -1,12 +1,50 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const https = require("https");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/failure", (req, res) => {
+  res.redirect("/");
+});
+
+app.post("/", (req, res) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: { FNAME: firstName, LNAME: lastName },
+      },
+    ],
+  };
+  const jsonData = JSON.stringify(data);
+  const url = "https://us21.api.mailchimp.com/3.0/lists/dd6a1026c7";
+  const option = {
+    method: "POST",
+    auth: "aditya:bb8f1d9560b1630eaf5271007e2260b8-us21",
+  };
+  const request = https.request(url, option, (response) => {
+    console.log(response);
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname / "/failure.html");
+    }
+  });
+  request.write(jsonData);
+  request.end();
+});
 
 app.listen(3000, () => {
   console.log("Server has started on port 3000");
